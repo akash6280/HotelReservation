02efcc907c5a8f94801f3105b7cd15b528b65a01
Akash Singh <akashsingh6280@gmail.com>
2021-09-17 03:54:20 +0530

    REFACTORED: finding cheapest hotel on weekday and weekend combined

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -1,23 +1,31 @@
 package com.bridgelabz.hotelreservation;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HotelReservation {
 	List<Hotel> hotelList=new ArrayList<>();
-	
+	int noOfWeekDay=0;
+	int noOfWeekEnd=0;
 	public boolean addHotel(Hotel hotel) {
 		hotelList.add(hotel);
 		return true;
 	}
 	
 	public Hotel cheapestHotel(LocalDate arrivalDate,LocalDate departureDate) {
-	
-		int stayPeriod = Period.between(arrivalDate, departureDate).getDays();
-		Hotel hotel=hotelList.stream().min((c1,c2)->c1.getWeekdayHotelRate()-c2.getWeekdayHotelRate()).orElse(null);
-		System.out.println("Cheapest hotel"+hotel.getWeekdayHotelRate()*stayPeriod);
-		return hotel;
+		
+		while(arrivalDate.compareTo(departureDate) <=0) {
+			if(arrivalDate.getDayOfWeek()==DayOfWeek.SATURDAY||arrivalDate.getDayOfWeek()==DayOfWeek.SUNDAY)
+				noOfWeekEnd++;
+			else
+				noOfWeekDay++;
+			arrivalDate=arrivalDate.plusDays(1);
+		}
+		Hotel cheapestHotel = hotelList.stream()
+							  .min((h1,h2)-> (h1.getWeekendHotelRate()*noOfWeekEnd+h1.getWeekdayHotelRate()*noOfWeekDay)-(h2.getWeekendHotelRate()*noOfWeekEnd+h2.getWeekdayHotelRate()*noOfWeekDay))
+							  .orElse(null);
+		return cheapestHotel;
 		
 	}
 }
