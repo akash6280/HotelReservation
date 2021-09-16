67e51fe8c6a95375a2854362461f54eb46d5cf36
Akash Singh <akashsingh6280@gmail.com>
2021-09-17 01:05:22 +0530

    ADDED: test case to return cheapest hotel

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.hotelreservation;
 
 import org.junit.Test;
+import java.time.LocalDate;
 import org.junit.Assert;
 
 public class HotelReservationTest {
@@ -10,6 +11,20 @@ public class HotelReservationTest {
 		HotelReservation hotelReservation=new HotelReservation();
 		boolean result=hotelReservation.addHotel(hotel);
 		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenDate_WhenFoundCheapestHotel_ShouldReturnHotel() {
+		HotelReservation hotelReservation=new HotelReservation();
+		Hotel hotel1=new Hotel("Lakewood",100);
+		hotelReservation.addHotel(hotel1);
+		Hotel hotel2=new Hotel("BridgeWood",200);
+		hotelReservation.addHotel(hotel2);
+		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
+		LocalDate departureDate = LocalDate.of(2020, 9, 11);
+		
+		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate);
+		Assert.assertEquals("Lakewood",hotel.getHotelName());
 	}
   
 }
