5a5a9e134625ded3608680d61a88a354ce0fbf48
Akash Singh <akashsingh6280@gmail.com>
2021-09-17 03:55:20 +0530

    ADDED: test case to return cheapest hotel on weekday and weekend combined

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 public class HotelReservationTest {
 	@Test
 	public void givenHotel_WhenAdded_ShouldReturnTrue() {
-		Hotel hotel=new Hotel("Lakewood",100,150);
+		Hotel hotel=new Hotel("LakeWood",110,150);
 		HotelReservation hotelReservation=new HotelReservation();
 		boolean result=hotelReservation.addHotel(hotel);
 		Assert.assertTrue(result);
@@ -16,15 +16,32 @@ public class HotelReservationTest {
 	@Test
 	public void givenDate_WhenFoundCheapestHotel_ShouldReturnHotel() {
 		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("Lakewood",100, 150);
+		Hotel hotel1=new Hotel("LakeWood",110, 90);
 		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",200, 250);
+		Hotel hotel2=new Hotel("BridgeWood",150, 50);
 		hotelReservation.addHotel(hotel2);
 		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
 		LocalDate departureDate = LocalDate.of(2020, 9, 11);
 		
 		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate);
-		Assert.assertEquals("Lakewood",hotel.getHotelName());
+		Assert.assertEquals("LakeWood",hotel.getHotelName());
+	}
+	
+	@Test
+	public void givenWeekendAndWeekDays_WhenFoundCheapestHotel_ShouldReturnHotel() {
+		HotelReservation hotelReservation=new HotelReservation();
+		Hotel hotel1=new Hotel("LakeWood",110, 90);
+		hotelReservation.addHotel(hotel1);
+		Hotel hotel2=new Hotel("BridgeWood",150, 50);
+		hotelReservation.addHotel(hotel2);
+		Hotel hotel3=new Hotel("RidgeWood",220, 150);
+		hotelReservation.addHotel(hotel3);
+		
+		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
+		LocalDate departureDate = LocalDate.of(2020, 9, 15);
+		
+		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate);
+		Assert.assertEquals("LakeWood",hotel.getHotelName());
 	}
   
 }
