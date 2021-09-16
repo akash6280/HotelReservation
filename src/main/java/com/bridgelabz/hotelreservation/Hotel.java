70baaf578a562d3ed5ddb4052b05f591f5baa0a8
Akash Singh <akashsingh6280@gmail.com>
2021-09-17 01:18:22 +0530

    REFACTORED: included getter and setter for weekend and weekday

--- src/main/java/com/bridgelabz/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -2,25 +2,38 @@ package com.bridgelabz.hotelreservation;
 
 public class Hotel{
 	    private String hotelName;
-	    private int hotelRate;
+	    private int weekdayHotelRate;
+	    private int weekendHotelRate;
 	    
 		public String getHotelName() {
 			return hotelName;
 		}
 		
-		public Hotel(String hotelName, int hotelRate) {
+		public Hotel(String hotelName, int weekdayHotelRate, int weekendHotelRate) {
 			super();
 			this.hotelName = hotelName;
-			this.hotelRate = hotelRate;
+			this.weekdayHotelRate = weekdayHotelRate;
+			this.weekendHotelRate = weekendHotelRate;
+		}
+
+		public int getWeekdayHotelRate() {
+			return weekdayHotelRate;
+		}
+
+		public void setWeekdayHotelRate(int weekdayHotelRate) {
+			this.weekdayHotelRate = weekdayHotelRate;
+		}
+
+		public int getWeekendHotelRate() {
+			return weekendHotelRate;
+		}
+
+		public void setWeekendHotelRate(int weekendHotelRate) {
+			this.weekendHotelRate = weekendHotelRate;
 		}
 
 		public void setHotelName(String hotelName) {
 			this.hotelName = hotelName;
 		}
-		public int getHotelRate() {
-			return hotelRate;
-		}
-		public void setHotelRate(int hotelRate) {
-			this.hotelRate = hotelRate;
-		}
+		
 	}
