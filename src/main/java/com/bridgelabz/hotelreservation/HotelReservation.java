af573667a0e6492fd488c0d844cc275352a947dc
Akash Singh <akashsingh6280@gmail.com>
2021-09-18 05:12:47 +0530

    REFACTORED : existing method to take customer type as parameter

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import com.bridgelabz.hotelreservation.HotelReservationException.exceptionType;
+
 public class HotelReservation {
 	List<Hotel> hotelList=new ArrayList<>();
 	int noOfWeekDay=0;
 	int noOfWeekEnd=0;
+	
 	public boolean addHotel(Hotel hotel) {
 		hotelList.add(hotel);
 		return true;
 	}
 	
-	public Hotel cheapestHotel(LocalDate arrivalDate,LocalDate departureDate) {
+	public Hotel cheapestHotel(LocalDate arrivalDate,LocalDate departureDate,CustomerType customerType) {
 		
 		while(arrivalDate.compareTo(departureDate) <=0) {
 			if(arrivalDate.getDayOfWeek()==DayOfWeek.SATURDAY||arrivalDate.getDayOfWeek()==DayOfWeek.SUNDAY)
@@ -23,24 +26,49 @@ public class HotelReservation {
 				noOfWeekDay++;
 			arrivalDate=arrivalDate.plusDays(1);
 		}
-		Hotel cheapestHotel = hotelList.stream()
-							  .min((h1,h2)-> (h1.getWeekendHotelRate()*noOfWeekEnd+h1.getWeekdayHotelRate()*noOfWeekDay)-(h2.getWeekendHotelRate()*noOfWeekEnd+h2.getWeekdayHotelRate()*noOfWeekDay))
-							  .orElse(null);
-		return cheapestHotel;
+		if(customerType==CustomerType.REGULAR) {
+			Hotel cheapestHotel = hotelList.stream()
+						    	  .min((h1,h2)-> (h1.getWeekendHotelRate()*noOfWeekEnd+h1.getWeekdayHotelRate()*noOfWeekDay)-(h2.getWeekendHotelRate()*noOfWeekEnd+h2.getWeekdayHotelRate()*noOfWeekDay))
+							      .orElse(null);
+	    	return cheapestHotel;
+		}
+		else {
+			Hotel cheapestHotel = hotelList.stream()
+								  .min((h1,h2)-> (h1.getRewardWeekEndHotelRate()*noOfWeekEnd+h1.getRewardWeekDayHotelRate()*noOfWeekDay)-(h2.getRewardWeekEndHotelRate()*noOfWeekEnd+h2.getRewardWeekDayHotelRate()*noOfWeekDay))
+								  .orElse(null);
+			return cheapestHotel;
+		}
+			
 		
 	}
 	
-	public Hotel cheapestAndBestRatedHotel(LocalDate arrivalDate,LocalDate departureDate) {
-	
-		Hotel cheapestHotel=cheapestHotel(arrivalDate, departureDate);
-		Hotel cheapestBestRatedHotel=hotelList.stream()
-								   .filter(h->(h.getWeekendHotelRate()*noOfWeekEnd+h.getWeekdayHotelRate()*noOfWeekDay)==(cheapestHotel.getWeekdayHotelRate()*noOfWeekDay+cheapestHotel.getWeekendHotelRate()*noOfWeekEnd))
-								   .max((h1,h2) -> h1.getHotelRating()-h2.getHotelRating())
-								   .orElse(null);
-		
-		return cheapestBestRatedHotel;
+	public Hotel cheapestAndBestRatedHotel(LocalDate arrivalDate,LocalDate departureDate,CustomerType customerType) {
+		try {	
+				if(arrivalDate.isAfter(departureDate))
+					throw new HotelReservationException(exceptionType.ENTERED_INVALID,"Entered wrong range of date");
+				
+				Hotel cheapestHotel=cheapestHotel(arrivalDate, departureDate,customerType);
+				if(customerType==CustomerType.REGULAR) {
+					Hotel cheapestBestRatedHotel=hotelList.stream()
+												 .filter(h->(h.getWeekendHotelRate()*noOfWeekEnd+h.getWeekdayHotelRate()*noOfWeekDay)==(cheapestHotel.getWeekdayHotelRate()*noOfWeekDay+cheapestHotel.getWeekendHotelRate()*noOfWeekEnd))
+									       	     .max((h1,h2) -> h1.getHotelRating()-h2.getHotelRating())
+										         .orElse(null);
+					return cheapestBestRatedHotel;
+				}
+				else {
+					Hotel cheapestBestRatedHotel=hotelList.stream()
+						                    	 .filter(h->(h.getRewardWeekDayHotelRate()*noOfWeekEnd+h.getRewardWeekEndHotelRate()*noOfWeekDay)==(cheapestHotel.getRewardWeekDayHotelRate()*noOfWeekDay+cheapestHotel.getRewardWeekEndHotelRate()*noOfWeekEnd))
+				       	                         .max((h1,h2) -> h1.getHotelRating()-h2.getHotelRating())
+					                             .orElse(null);
+					return cheapestBestRatedHotel;
+					
+				}
+					
+		} 
+		catch (NullPointerException e) {
+			throw new HotelReservationException(exceptionType.ENTERED_NULL,"entered Null value");
+		}
 	}
-	
 	public Hotel getBestRatedHotel(LocalDate arrivalDate,LocalDate departureDate) {
 		
 		while(arrivalDate.compareTo(departureDate) <=0) {
