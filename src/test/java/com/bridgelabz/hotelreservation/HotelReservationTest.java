214109f1b2f20f0439ec5f4e646a5730a86be4d2
Akash Singh <akashsingh6280@gmail.com>
2021-09-18 05:42:06 +0530

    ADDED : test case to throw exception when date range is invalid

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -122,6 +122,7 @@ public class HotelReservationTest {
 			Assert.assertEquals(exceptionType.ENTERED_NULL,e.etype);
 		}
 	}
+	
 	@Test
 	public void givenDateRange_WhenDepartureDateIsNull_ShouldThrowException() {
 		HotelReservation hotelReservation=new HotelReservation();
@@ -137,6 +138,24 @@ public class HotelReservationTest {
 			hotelReservation.cheapestAndBestRatedHotel(arrivalDate,null,CustomerType.REWARD);
 		} catch (HotelReservationException e) {
 			Assert.assertEquals(exceptionType.ENTERED_NULL,e.etype);
+		}
+	}
+	
+	@Test
+	public void givenDateRange_WhenArrivalDateIsAfterDepartureDate_ShouldThrowException() {
+		HotelReservation hotelReservation=new HotelReservation();
+		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
+		hotelReservation.addHotel(hotel1);
+		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
+		hotelReservation.addHotel(hotel2);
+		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
+		hotelReservation.addHotel(hotel3);
+		LocalDate arrivalDate = LocalDate.of(2020, 9, 15);
+		LocalDate departureDate = LocalDate.of(2020, 9, 10);
+		try {
+			hotelReservation.cheapestAndBestRatedHotel(arrivalDate,departureDate,CustomerType.REWARD);
+		} catch (HotelReservationException e) {
+			Assert.assertEquals(exceptionType.ENTERED_INVALID,e.etype);		
 		}
 	}
   
