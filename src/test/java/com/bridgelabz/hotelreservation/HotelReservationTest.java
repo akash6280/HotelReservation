231d621354520dd54c62c9dc534cf62184380c2d
Akash Singh <akashsingh6280@gmail.com>
2021-09-18 07:20:03 +0530

    REFACTORED: use of @before to initialise objects before every test case

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -6,25 +6,35 @@ import com.bridgelabz.hotelreservation.HotelReservationException.exceptionType;
 
 import java.time.LocalDate;
 import org.junit.Assert;
+import org.junit.Before;
 
 public class HotelReservationTest {
+	LocalDate arrivalDate;
+	LocalDate departureDate;
+	HotelReservation hotelReservation;
+	
+	@Before
+	public void initialise() {
+		hotelReservation=new HotelReservation();
+		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
+		hotelReservation.addHotel(hotel1);
+		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
+		hotelReservation.addHotel(hotel2);
+		Hotel hotel3=new Hotel("LakeWood",220, 150,5,100,40);
+		hotelReservation.addHotel(hotel3);
+		arrivalDate = LocalDate.of(2020, 9, 10);
+	    departureDate = LocalDate.of(2020, 9, 11);
+	}
+	
 	@Test
 	public void givenHotel_WhenAdded_ShouldReturnTrue() {
 		Hotel hotel=new Hotel("LakeWood",110,150,3,80,80);
-		HotelReservation hotelReservation=new HotelReservation();
 		boolean result=hotelReservation.addHotel(hotel);
 		Assert.assertTrue(result);
 	}
 	
 	@Test
 	public void givenDate_WhenFoundCheapestHotel_ShouldReturnHotel() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 11);
 		
 		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate,CustomerType.REGULAR);
 		Assert.assertEquals("LakeWood",hotel.getHotelName());
@@ -32,17 +42,7 @@ public class HotelReservationTest {
 	
 	@Test
 	public void givenWeekendAndWeekDays_WhenFoundCheapestHotel_ShouldReturnHotel() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
-		
+
 		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate,CustomerType.REWARD);
 		Assert.assertEquals("LakeWood",hotel.getHotelName());
 	}
@@ -50,8 +50,6 @@ public class HotelReservationTest {
 	@Test
 	public void givenWeekendAndWeekDays_WhenNoHotel_ShouldReturnNull() {
 		HotelReservation hotelReservation=new HotelReservation();
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
 		
 		Hotel hotel=hotelReservation.cheapestHotel(arrivalDate, departureDate,CustomerType.REWARD);
 		Assert.assertNull(hotel);
@@ -59,15 +57,7 @@ public class HotelReservationTest {
 	
 	@Test
 	public void givenDateRange_WhenFoundCheapestAndBestRatedHotel_ShouldReturnHotel() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",110, 90,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
+		
 		Hotel hotel=hotelReservation.cheapestAndBestRatedHotel(arrivalDate, departureDate,CustomerType.REWARD);
 		Assert.assertEquals("LakeWood",hotel.getHotelName());
 	}
@@ -75,46 +65,26 @@ public class HotelReservationTest {
 	@Test
 	public void givenDateRange_WhenNotFoundCheapestAndBestRatedHotel_ShouldReturnNull() {
 		HotelReservation hotelReservation=new HotelReservation();
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
 		Hotel hotel=hotelReservation.cheapestAndBestRatedHotel(arrivalDate, departureDate,CustomerType.REWARD);
 		Assert.assertEquals(null,hotel);
 	}
 	
 	@Test
 	public void givenDateRange_WhenFoundBestRatedHotel_ShouldReturnHotel() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
+		
 		Hotel hotel=hotelReservation.getBestRatedHotel(arrivalDate, departureDate);
-		Assert.assertEquals("RidgeWood",hotel.getHotelName());
+		Assert.assertEquals("LakeWood",hotel.getHotelName());
 	}
 	
 	@Test
 	public void givenDateRange_WhenNoBestRatedHotelFound_ShouldReturnNull() {
 		HotelReservation hotelReservation=new HotelReservation();
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
 		Hotel hotel=hotelReservation.getBestRatedHotel(arrivalDate, departureDate);
 		Assert.assertNull(hotel);
 	}
 	
 	@Test
 	public void givenDateRange_WhenArrivaLDateIsNull_ShouldThrowException() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		LocalDate departureDate = LocalDate.of(2020, 9, 15);
 		
 		try {
 			hotelReservation.cheapestAndBestRatedHotel(null, departureDate,CustomerType.REWARD);
@@ -125,14 +95,6 @@ public class HotelReservationTest {
 	
 	@Test
 	public void givenDateRange_WhenDepartureDateIsNull_ShouldThrowException() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 10);
 		
 		try {
 			hotelReservation.cheapestAndBestRatedHotel(arrivalDate,null,CustomerType.REWARD);
@@ -143,13 +105,6 @@ public class HotelReservationTest {
 	
 	@Test
 	public void givenDateRange_WhenArrivalDateIsAfterDepartureDate_ShouldThrowException() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
 		LocalDate arrivalDate = LocalDate.of(2020, 9, 15);
 		LocalDate departureDate = LocalDate.of(2020, 9, 10);
 		try {
@@ -161,15 +116,6 @@ public class HotelReservationTest {
 	
 	@Test
 	public void givenDateRange_WhenCustomerFieldIsNull_ShouldThrowException() {
-		HotelReservation hotelReservation=new HotelReservation();
-		Hotel hotel1=new Hotel("LakeWood",110, 90,3,80,80);
-		hotelReservation.addHotel(hotel1);
-		Hotel hotel2=new Hotel("BridgeWood",150, 50,4,110,50);
-		hotelReservation.addHotel(hotel2);
-		Hotel hotel3=new Hotel("RidgeWood",220, 150,5,100,40);
-		hotelReservation.addHotel(hotel3);
-		LocalDate arrivalDate = LocalDate.of(2020, 9, 15);
-		LocalDate departureDate = LocalDate.of(2020, 9, 10);
 		try {
 			hotelReservation.cheapestAndBestRatedHotel(arrivalDate,departureDate,null);
 		} catch (HotelReservationException e) {
